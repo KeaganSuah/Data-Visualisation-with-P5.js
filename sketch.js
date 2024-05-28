dea164cca8981609949f414e198b9dacd4b09d82
Keagan Suah <suahkeagan@gmail.com>
2024-05-29 00:22:04 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup() {
   gallery.addVisual(new PayGapByJob2017());
   gallery.addVisual(new PayGapTimeSeries());
   gallery.addVisual(new ClimateChange());
+  gallery.addVisual(new NutrientsTimeSeries())
 }
 
 function draw() {
